d94f10343b5910ceedd7bdfe4181d4f4527f73f1
M <mtolive2@asu.edu>
2024-06-11 17:08:37 +0100

    initial commit

---
+++ adsb_process.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 16 * 163834
+
+void process_adsb_data(uint8_t *buffer, int length){
+  // write function to process raw ADS-B data
+  // Implement ADS-B decoding logic
+  printf("Recieved data chunk of size %d\n", length);
+  for(int i = 0; i < length; i++){
+     printf("%02X ", buffer[i]);
+  }
+  printf("\n");
+}
+
+int main(){
+ uint8_t buffer[BUFFER_SIZE];
+ ssize_t len;
+
+  while((len = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
+    process_adsb_data(buffer, len);
+  }
+
+  if(len < 0) {
+    perror("read");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
---
+++ bit_stream_analyzer.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define BUFFER_SIZE 16384  // 16KB
+#define FRAME_SIZE 14 // ADS-B frame size is 14 bytes
+
+// handle exit
+volatile sig_atomic_t stop = 0;
+
+void handle_sigint(int signum){
+   stop = 1;
+}
+
+// right shift i to least significant bit mask using 1.
+void print_bits(uint8_t byte) {
+   for(int i = 7; i >= 0; i--){
+      printf("%d", (byte >> i) & 1); 
+   }
+}
+
+void analyze(uint8_t *buffer, int len){
+  
+   if(len <  FRAME_SIZE){
+      return;
+   }
+
+   // Extract Downlink Format (DF) from the first byte
+   uint8_t df = (buffer[0] >> 3) & 0x1F; // first 5 bits
+    
+   // check that DF is 17
+   if(df == 17){
+   
+      // print hex value
+      for(int i = 0; i < FRAME_SIZE; i++){
+         printf("%02X", buffer[i]);
+      }
+      printf("\n");
+
+      // print bits
+      /*
+      for(int i = 0; i < FRAME_SIZE; i++){
+         print_bits(buffer[i]); // pass in each byte
+         printf(" ");
+      }
+      printf("\n");
+      */
+   }
+}
+
+int main(){
+   uint8_t buffer[BUFFER_SIZE];
+   ssize_t len;
+  
+   while (!stop && (len = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0){
+     analyze(buffer, len);
+   }
+  // capture a single frame
+  /* if((len = read(STDIN_FILENO, buffer, FRAME_SIZE)) > 0){
+     analyze(buffer, len);
+   }*/
+
+   if(len < 0){
+      perror("read");
+      return EXIT_FAILURE;
+   }
+}
