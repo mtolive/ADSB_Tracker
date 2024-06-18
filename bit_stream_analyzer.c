afbf260599b6686e4dfb17e4ee1967451f75e350
M <mtolive2@asu.edu>
2024-06-18 16:01:19 +0100

    updated code

--- bit_stream_analyzer.c
+++ bit_stream_analyzer.c
@@ -21,31 +21,49 @@ void print_bits(uint8_t byte) {
    }
 }
 
+// print bits
+void print_bin(uint8_t *buffer, int len){   
+   for(int i = 0; i < len; i++){
+      print_bits(buffer[i]); // pass in each byte
+      printf(" ");
+   }
+   
+   printf("\n");   
+}
+
+// print hex
+void print_hex(uint8_t *buffer, int len){
+   for(int i = 0; i < len; i++){
+      printf("%02X", buffer[i]);
+   }
+   
+   printf("\n");
+}
+
 void analyze(uint8_t *buffer, int len){
-  
    if(len <  FRAME_SIZE){
       return;
    }
-
    // extract downlink format (DF) from the first byte
    uint8_t df = (buffer[0] >> 3) & 0x1F; // first 5 bits
-    
-   if(df == 17){
-   
-      for(int i = 0; i < FRAME_SIZE; i++){
-         printf("%02X", buffer[i]);
-      }
-      printf("\n");
+   // extract type code (TC) from byte 5
+   uint8_t tc = (buffer[4] >> 3) & 0x1F; // bits 33-37    
 
-      // print bits
-      /*
-      for(int i = 0; i < FRAME_SIZE; i++){
-         print_bits(buffer[i]); // pass in each byte
-         printf(" ");
-      }
-      printf("\n");
-      */
+   if(df == 17){
+      print_hex(buffer, FRAME_SIZE);
    }
+}
+
+void capture_single_frame(uint8_t *buffer, int len){
+  // len is FRAME_SIZE
+  ssize_t frame;
+  if((frame = read(STDIN_FILENO, buffer, len)) > 0){
+   analyze(buffer, len);
+  }
+  
+  if(len < 0){
+      perror("read");
+  }
 }
 
 int main(){
@@ -55,10 +73,6 @@ int main(){
    while (!stop && (len = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0){
      analyze(buffer, len);
    }
-  // capture a single frame
-  /* if((len = read(STDIN_FILENO, buffer, FRAME_SIZE)) > 0){
-     analyze(buffer, len);
-   }*/
 
    if(len < 0){
       perror("read");
